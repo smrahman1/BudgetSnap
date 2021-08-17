44081f0df4beec4fad128c0a9b1d614c7d5c0994
Sheikh Mahfuzur Rahman <smrahman5566@hwdsb.on.ca>
2021-08-17 14:05:08 -0400

    Updated Settings

--- BudgetTracker/settings.py
+++ BudgetTracker/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#9jc%0)dx(#g$-i@2-t8u^06@=wd08px=pd@s!q=5+$veuyx#'
+#SECRET_KEY = 'django-insecure-n#9jc%0)dx(#g$-i@2-t8u^06@=wd08px=pd@s!q=5+$veuyx#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['budgetsnap.herokuapp.com/']
 
 
 # Application definition
@@ -121,6 +122,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
