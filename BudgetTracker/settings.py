fd4feb7135367d30ff3cf28820315feb828ec407
Sheikh Mahfuzur Rahman <smrahman5566@hwdsb.on.ca>
2021-08-21 20:51:04 -0400

    Added django-heroku

--- BudgetTracker/settings.py
+++ BudgetTracker/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+import django_heroku
+
 from datetime import datetime
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +142,5 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
